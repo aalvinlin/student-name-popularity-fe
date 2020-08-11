3d0780f8749496a2b0e3d29279b6524d1beedd08
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-08-10 23:46:58 -0700

    create Menu and ProgressBar components

---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Menu = () => {
+
+    return (
+        <header>
+            <a href="#">Home</a>
+            <a href="#">Generate Worksheets</a>
+        </header>
+    );
+}
+
+export default Menu;
---
+++ src/components/ProgressBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProgressBar = () => {
+
+    return (
+        <div className="progressBar">
+
+        </div>
+    );
+}
+
+export default ProgressBar;
