707568ca069d3b84d61545bd6bd0dbf6b848cb74
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-08-19 23:51:09 -0700

    fix JSX syntax and imports for ModalEditWorksheet

--- src/components/ModalEditWorksheet.js
+++ src/components/ModalEditWorksheet.js
@@ -1,21 +1,30 @@
 import React from "react";
 
+import EditWorksheetHeader from "./EditWorksheetHeader";
+import EditIntro from "./EditIntro";
+import EditFiveNumberSummary from "./EditFiveNumberSummary";
+import EditHistogram from "./EditHistogram";
+import EditQuestions from "./EditQuestions";
+
 const ModalEditWorksheet = ({page}) => {
 
     if (page === 1)
         {
             return (
-
-                <EditWorksheetHeader />
-                <EditIntro />
-                <EditFiveNumberSummary />
+                <>
+                    <EditWorksheetHeader />
+                    <EditIntro />
+                    <EditFiveNumberSummary />
+                </>
             )
         }
     else if (page === 1)
         {
             return (
-                <EditHistogram />
-                <EditQuestions />
+                <>
+                    <EditHistogram />
+                    <EditQuestions />
+                </>
             )
         }
 }
