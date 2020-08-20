36a37490eae9d4ac837774a63d9c4baf0934acff
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-08-19 23:18:47 -0700

    add div structure for EditWorksheet component

--- src/components/EditWorksheet.js
+++ src/components/EditWorksheet.js
@@ -4,6 +4,34 @@ const EditWorksheet = () => {
 
     return (
         <>
+            <h2>Edit Worksheet</h2>
+
+            <div className="editWorksheet">
+
+                <div className="pageDisplay">
+                    <h3>Page 1</h3>
+                    <div className="pageContents">
+
+                    </div>
+
+                    <button className="edit">Edit</button>
+
+                </div>
+
+                <div className="pageDisplay">
+                    <h3>Page 2</h3>
+                    <div className="pageContents">
+
+                    </div>
+
+                    <button className="edit">Edit</button>
+
+                </div>
+
+                <button className="addNames">Add Student Names</button>
+                    
+            </div>
+
         </>
     );
 }
